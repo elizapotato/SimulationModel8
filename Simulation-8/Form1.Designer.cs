1d2ead06a15cdd08fc3682b80f1ea614b401a01e
Elizaveta <42829488+sexypotato@users.noreply.github.com>
2021-06-04 03:07:24 +0700

    Add files via upload

---
+++ Simulation-8/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿
+namespace Simulation_8
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.labBall = new System.Windows.Forms.Label();
+            this.questBox = new System.Windows.Forms.TextBox();
+            this.ansButton = new System.Windows.Forms.Button();
+            this.labMode = new System.Windows.Forms.Label();
+            this.checkMode = new System.Windows.Forms.CheckBox();
+            this.labAns = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::Simulation_8.Properties.Resources.magiya1;
+            this.pictureBox1.Location = new System.Drawing.Point(-120, -137);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(45);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(999, 835);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // labBall
+            // 
+            this.labBall.AutoSize = true;
+            this.labBall.BackColor = System.Drawing.Color.NavajoWhite;
+            this.labBall.Font = new System.Drawing.Font("Jokerman", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.labBall.Location = new System.Drawing.Point(316, 48);
+            this.labBall.Name = "labBall";
+            this.labBall.Padding = new System.Windows.Forms.Padding(5);
+            this.labBall.Size = new System.Drawing.Size(164, 47);
+            this.labBall.TabIndex = 1;
+            this.labBall.Text = "Magic Ball";
+            // 
+            // questBox
+            // 
+            this.questBox.BackColor = System.Drawing.Color.White;
+            this.questBox.Location = new System.Drawing.Point(245, 152);
+            this.questBox.Name = "questBox";
+            this.questBox.Size = new System.Drawing.Size(303, 27);
+            this.questBox.TabIndex = 2;
+            // 
+            // ansButton
+            // 
+            this.ansButton.BackColor = System.Drawing.Color.SandyBrown;
+            this.ansButton.FlatAppearance.BorderColor = System.Drawing.Color.Black;
+            this.ansButton.FlatAppearance.MouseDownBackColor = System.Drawing.Color.DarkOrange;
+            this.ansButton.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(245)))), ((int)(((byte)(180)))), ((int)(((byte)(120)))));
+            this.ansButton.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.ansButton.Font = new System.Drawing.Font("Jokerman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.ansButton.Location = new System.Drawing.Point(304, 213);
+            this.ansButton.Name = "ansButton";
+            this.ansButton.Size = new System.Drawing.Size(189, 54);
+            this.ansButton.TabIndex = 3;
+            this.ansButton.Text = "Get an answer";
+            this.ansButton.UseVisualStyleBackColor = false;
+            this.ansButton.Click += new System.EventHandler(this.ansButton_Click);
+            // 
+            // labMode
+            // 
+            this.labMode.AutoSize = true;
+            this.labMode.BackColor = System.Drawing.Color.NavajoWhite;
+            this.labMode.Font = new System.Drawing.Font("Jokerman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.labMode.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.labMode.Location = new System.Drawing.Point(287, 469);
+            this.labMode.Name = "labMode";
+            this.labMode.Padding = new System.Windows.Forms.Padding(5);
+            this.labMode.Size = new System.Drawing.Size(175, 39);
+            this.labMode.TabIndex = 4;
+            this.labMode.Text = "YES/NO mode";
+            // 
+            // checkMode
+            // 
+            this.checkMode.AutoSize = true;
+            this.checkMode.BackColor = System.Drawing.Color.NavajoWhite;
+            this.checkMode.Location = new System.Drawing.Point(459, 469);
+            this.checkMode.Name = "checkMode";
+            this.checkMode.Padding = new System.Windows.Forms.Padding(11);
+            this.checkMode.Size = new System.Drawing.Size(40, 39);
+            this.checkMode.TabIndex = 5;
+            this.checkMode.UseVisualStyleBackColor = false;
+            this.checkMode.CheckedChanged += new System.EventHandler(this.checkMode_CheckedChanged);
+            // 
+            // labAns
+            // 
+            this.labAns.AutoSize = true;
+            this.labAns.BackColor = System.Drawing.Color.NavajoWhite;
+            this.labAns.Font = new System.Drawing.Font("Jokerman", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.labAns.Location = new System.Drawing.Point(287, 309);
+            this.labAns.Name = "labAns";
+            this.labAns.Padding = new System.Windows.Forms.Padding(5);
+            this.labAns.Size = new System.Drawing.Size(10, 43);
+            this.labAns.TabIndex = 6;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(785, 555);
+            this.Controls.Add(this.labAns);
+            this.Controls.Add(this.checkMode);
+            this.Controls.Add(this.labMode);
+            this.Controls.Add(this.ansButton);
+            this.Controls.Add(this.questBox);
+            this.Controls.Add(this.labBall);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "MAGIC BALL";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label labBall;
+        private System.Windows.Forms.TextBox questBox;
+        private System.Windows.Forms.Button ansButton;
+        private System.Windows.Forms.Label labMode;
+        private System.Windows.Forms.CheckBox checkMode;
+        private System.Windows.Forms.Label labAns;
+    }
+}
+
---
+++ Simulation-8/Form1.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Simulation_8
+{
+    public partial class Form1 : Form
+    {
+        MagicBall ball = new MagicBall();
+        string prevText = "";
+        public Form1()
+        {
+            InitializeComponent();
+            labAns.Text = "";
+        }
+
+        public class MagicBall
+        {
+            private readonly Dictionary<int, string> ans = new Dictionary<int, string>()
+            {
+                {0, "It is certain."},
+                {1, "Without a doubt."},
+                {2, "You may rely on it."},
+                {3, "It is decidedly so."},
+                {4, "Signs point to yes."},
+
+                {5, "Don’t count on it."},
+                {6, "Very doubtful."},
+                {7, "No, no, definitely not."},
+                {8, "It is absolutely not."},
+                {9, "Outlook not so good."},
+
+                {10, "Cannot predict now."},
+                {11, "Ask again later."},
+            };
+
+            private readonly Dictionary<int, double> prob = new Dictionary<int, double>()
+            {
+                {0, 0.08},
+                {1, 0.1},
+                {2, 0.11},
+                {3, 0.09},
+                {4, 0.07},
+
+                {5, 0.08},
+                {6, 0.1},
+                {7, 0.11},
+                {8, 0.09},
+                {9, 0.07},
+
+                {10, 0.06},
+                {11, 0.04},
+            };
+
+            public string GetAnswer()
+            {
+                Random random = new Random();
+                double chance = random.NextDouble();
+                for (var i = 0; i <= prob.Count; i++)
+                {
+                    chance -= prob[i];
+                    if (chance <= 0)
+                        return ans[i];
+                };
+                return ans[0];
+            }
+        }
+
+        private void checkMode_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void ansButton_Click(object sender, EventArgs e)
+        {
+            if (questBox.Text == "")
+            {
+                labAns.Text = "Ask the question!";
+                return;
+            }
+            if (questBox.Text == prevText)
+            {
+                labAns.Text = "Ask the new question!";
+                return;
+            }
+            else 
+            {
+                if (checkMode.Checked)
+                {
+                    Random random = new Random();
+                    if (random.NextDouble() < 0.5) labAns.Text = "Yes";
+                    else labAns.Text = "No";
+                    prevText = questBox.Text;
+                    return;
+                }
+                else
+                {
+                    labAns.Text = ball.GetAnswer();
+                    prevText = questBox.Text;
+                    return;
+                }
+            }
+            
+        }
+    }
+}
